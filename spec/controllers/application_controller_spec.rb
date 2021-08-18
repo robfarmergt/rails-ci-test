0a13a730d7561c1e7a6978135cc7ad704de42217
Rob Farmer <robfarmergt@users.noreply.github.com>
2021-08-18 10:54:52 -0400

    fix hound violations

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe ApplicationController do
-  it 'runs a truthy test' do
-  expect(true).to be_truthy
+  it "runs a truthy test" do
+    expect(true).to be_truthy
   end
 end
