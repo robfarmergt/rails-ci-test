f556c5c55f019b3d02eb8c009a3166b721f50f37
Rob Farmer <robfarmergt@users.noreply.github.com>
2021-08-19 15:04:07 -0400

    remove build step

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,8 +6,4 @@ describe ApplicationController do
   it "runs a truthy test" do
     expect(true).to be_truthy
   end
-
-  it "lets see what happens with a failure" do
-    expect(false).to be_truthy
-  end
 end
