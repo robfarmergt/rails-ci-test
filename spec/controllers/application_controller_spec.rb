4af49eed8ec6b4af803efc8acd6af43674d3397e
Rob Farmer <robfarmergt@users.noreply.github.com>
2021-08-19 14:55:40 -0400

    falsy test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,4 +6,8 @@ describe ApplicationController do
   it "runs a truthy test" do
     expect(true).to be_truthy
   end
+
+  it "lets see what happens with a failure" do
+    expect(false).to be_truthy
+  end
 end
